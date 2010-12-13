30a1efb7ecffbf6ded63b083a01252785ca0cf38
7rans <transfire@gmail.com>
2010-12-12 22:04:53 -0500

    admin: update metadata for latest pom

--- lib/autoreload/version.rb
+++ lib/autoreload/version.rb
@@ -1,19 +1,3 @@
 module AutoReload
-  #
-  def self.version
-    @version ||= (
-      require 'yaml'
-      YAML.load(File.new(File.dirname(__FILE__) + '/version.yml'))
-    )
-  end
-
-  #
-  def self.const_missing(name)
-    key = name.to_s.downcase
-    version[key] || super(name)
-  end
-
-  # becuase Ruby 1.8~ gets in the way
-  VERSION = version['version']
+  VERSION = '0.3.0'
 end
-
