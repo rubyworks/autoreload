c4939741dbc23147371ccbff0062a7a14891dc2b
7rans <transfire@gmail.com>
2011-07-15 21:06:57 -0400

    Add require of require test. [test]

--- spec/autoreload_spec.rb
+++ spec/autoreload_spec.rb
@@ -60,4 +60,35 @@ describe "AutoReload" do
     library.unlink
   end
 
+  it "should autoreload a require of a require" do
+    # create a library
+    library1 = Pathname.new('tmp/library1.rb')
+    library2 = Pathname.new('tmp/library2.rb')
+    library1.write "require 'library2'"
+    library2.write "def foo; 1; end"
+
+    # setup the autoreload
+    autoreload(:interval => 1) do #, :verbose=>true)
+      require "library1"
+    end
+
+    # check the number
+    foo.must_equal 1
+
+    # wait is needed for time stamp to not be same with the next file.
+    sleep 2
+
+    # recreate the file
+    library2.write 'def foo; 2; end'
+
+    # wait again for the autoreload loop to repeat.
+    sleep 2
+
+    # check the number again
+    foo.must_equal 2
+
+    # clean up
+    library1.unlink
+    library2.unlink
+  end
 end
